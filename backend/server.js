4d1fc0cc93bdd2e75991f7cf1657eec38afb7a63
Mackowi <dzierzenga.maciej@gmail.com>
2023-05-22 18:19:41 +0200

    deployment prep

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+const path = require('path')
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const e = require('express');
 
 connectDB();
 
@@ -14,6 +16,16 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+// serve frontent
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index'))
+  })
+} else {
+  app.get('/', (req, res) => res.send('Please set env variable to production'))
+}
 
 app.use(errorHandler);
 
